0d43ffaf2e958f9f9e8e5dad501167aa42872afa
donggenmu <chenyinjiewj@gmail.com>
2021-03-01 16:39:07 +0800

    一些环境配置

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,15 @@
+import React from "react";
 import Head from "next/head";
-
+import { Button } from "antd";
 const Home = () => {
   return (
     <div>
       <Head>
         <title>华师计算机学院智能推荐</title>
       </Head>
+      <div>
+        <Button>test</Button>
+      </div>
     </div>
   );
 };
