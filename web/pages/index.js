51519666adbcb417a32bbde6660177a304d88eec
donggenmu <chenyinjiewj@gmail.com>
2021-03-01 16:07:34 +0800

    前台init

---
+++ web/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>华师计算机学院智能推荐</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
