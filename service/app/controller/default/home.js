79831164b5d0e805194b3a5c2b507dde6f687d69
donggenmu <chenyinjiewj@gmail.com>
2021-03-04 18:14:22 +0800

    git init

---
+++ service/app/controller/default/home.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const Controller = require("egg").Controller;
+
+class HomeController extends Controller {
+  async index() {
+    // this.ctx.body = "api接口";
+    let result = await this.app.mysql.get("article", {});
+
+    this.ctx.body = result;
+  }
+
+  async getArticleList() {
+    let sql =
+      "SELECT article.id as id, " +
+      "article.title as title, " +
+      "article.type as type, " +
+      "article.introduce as introduce, " +
+      "article.content as content, " +
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime " +
+      "from article";
+    const results = await this.app.mysql.query(sql);
+
+    this.ctx.body = {
+      data: results,
+    };
+  }
+}
+
+module.exports = HomeController;
---
+++ service/config/config.default.js
@@ -0,0 +1,50 @@
+/* eslint valid-jsdoc: "off" */
+
+"use strict";
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = (appInfo) => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + "_1614844590240_8969";
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: "121.4.120.180",
+      // port
+      port: "3306",
+      // username
+      user: "dgm",
+      // password
+      password: "ZXCasdqwe123!@#",
+      // database
+      database: "KGRS",
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
