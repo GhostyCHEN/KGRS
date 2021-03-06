8c72e8c66b8083e3bef2de29585d3949b7b18524
donggenmu <chenyinjiewj@gmail.com>
2021-03-06 14:50:33 +0800

    管理后台框架搭建

---
+++ admin/src/Pages/Login.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, createContext } from "react";
+import { Card, Input, Button, Spin, message } from "antd";
+import { Icon } from "@ant-design/compatible";
+import axios from "axios";
+
+function Login() {
+  return <div>111</div>;
+}
+
+export default Login;
